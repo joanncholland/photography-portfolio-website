2cfb8e84c4b97ef85bc6b51a2816dde7d4adad2b
Joann Holland <joann.c.holland@btinternet.com>
2018-09-23 12:04:35 +0100

    Created image flexbox and added title.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-}
+// function toggleNav(x) {
+//     x.classList.toggle("change");
+//     if(document.getElementById("myNav").style.height == "100%"){
+//       document.getElementById("myNav").style.height = "0%";
+//     } else {
+//       document.getElementById("myNav").style.height == "100%";
+//     }
+// }
