d8fee7e315755588c0b86d8bc1c27262cba6430d
Joann Holland <>
2018-09-25 12:02:35 +0100

    Added breakpoints.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
-function myFunction(x) {
-    x.classList.toggle("change");
-}
-=======
-
-
-// function cross(x) {
-//     x.classList.toggle("change");
-// }
-
-var menuicon = document.getElementsByClassName('menu-icon');
-
-var bar1 = document.getElementsByClassName('bar1');
-var bar2 = document.getElementsByClassName('bar2');
-var bar3 = document.getElementsByClassName('bar3');
-
-menuicon[0].addEventListener('click', function(){
-  bar1[0].toggle("change");
-  bar2[0].toggle("change");
-  bar3[0].toggle("change");
-  var menu = document.getElementById('menu');
-  if(menu.style.display === "none"){
-    menu.style.display = "block";
-  } else {
-    menu.style.display = "none";
-  }
-
+$('.menu-icon').click(function(){
+  $('ul').slideToggle(400);
+  $('.menu-icon').toggleClass('change');
 });
->>>>>>> c2577e584ca1222fe91b205a7bc72c30e546c516
